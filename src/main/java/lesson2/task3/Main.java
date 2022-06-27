ed9883f0c4a813db7f299d216e267f7a5c88d57e
idel <idel_11@me.com>
2022-06-27 15:14:59 +0300

    Lesson2, task3 - implementation

---
+++ src/main/java/lesson2/task3/Main.java
@@ -0,0 +1,28 @@
+package lesson2.task3;
+
+import java.util.*;
+
+public class Main {
+
+    public static List<Integer> getIntegerList() {
+        List<Integer> list = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number(Integer): ");
+        int number = sc.nextInt();
+        for (int i = 0; i < number; i++) {
+            System.out.print("Enter number(Integer): ");
+            list.add(i, sc.nextInt());
+        }
+        return list;
+    }
+
+    public static Integer getMinimum(List<Integer> list) {
+        return Collections.min(list);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Min value is " + getMinimum(getIntegerList()));
+
+    }
+}
