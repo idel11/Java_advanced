cf8ff34acd8bdf5a7fdebfe19aa182eb1d632e66
idel <idel_11@me.com>
2022-06-27 14:50:39 +0300

    Lesson2, task2 - small changes

--- src/main/java/lesson2/task2/Main.java
+++ src/main/java/lesson2/task2/Main.java
@@ -4,25 +4,28 @@ import java.util.*;
 
 public class Main {
 
-    private static LinkedList<String> words = new LinkedList<>();
-    public static LinkedList<String> fillLinked(LinkedList<String> words){
+    private static List<String> words = new LinkedList<>();
+
+    public static List<String> fill(List<String> words) {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < 5; i++) {
             System.out.print("Enter word #" + i + " : ");
             String s = sc.nextLine();
-            words.addLast(s);
-        } return words;
+            words.add(s);
+        }
+        return words;
     }
 
-    public static LinkedList<String> doubleValues(LinkedList<String> words){
+    public static List<String> doubleValues(List<String> words) {
         int temp = ((words.size() * 2) - 1);
         for (int i = 0; i < temp; i++) {
             words.add((i + 1), words.get(i));
             i++;
-        } return words;
+        }
+        return words;
     }
 
-    public static void printLinked(LinkedList<String> words){
+    public static void print(List<String> words) {
         for (int i = 0; i < words.size(); i++) {
             System.out.println(words.get(i));
         }
@@ -30,11 +33,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        fillLinked(words);
+        fill(words);
 
         doubleValues(words);
 
-        printLinked(words);
+        print(words);
 
     }
 }
