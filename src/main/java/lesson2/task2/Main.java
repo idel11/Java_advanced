1cbb076e28360595e3250fac96b79ac459b2d8ed
idel <idel_11@me.com>
2022-06-27 14:29:47 +0300

    Lesson2, task2 - implemented

--- src/main/java/lesson2/task2/Main.java
+++ src/main/java/lesson2/task2/Main.java
@@ -1,24 +1,40 @@
 package lesson2.task2;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    private static LinkedList<String> words = new LinkedList<>();
+    public static LinkedList<String> fillLinked(LinkedList<String> words){
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter word #" + i + " : ");
+            String s = sc.nextLine();
+            words.addLast(s);
+        } return words;
+    }
+
+    public static LinkedList<String> doubleValues(LinkedList<String> words){
+        int temp = ((words.size() * 2) - 1);
+        for (int i = 0; i < temp; i++) {
+            words.add((i + 1), words.get(i));
+            i++;
+        } return words;
+    }
+
+    public static void printLinked(LinkedList<String> words){
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println(words.get(i));
+        }
+    }
+
     public static void main(String[] args) {
 
-        Collection<String> words = new ArrayList<>();
+        fillLinked(words);
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter five words separated by space: ");
-        String s  = sc.nextLine();
-        words = List.of(s.split(" "));
+        doubleValues(words);
 
-        System.out.println(words);
-
-
-
+        printLinked(words);
 
     }
 }
