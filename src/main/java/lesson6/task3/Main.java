64889be9a9653d73927603be92084682a57be796
dmytro_lyshtvan <idel_11@me.com>
2022-07-13 22:23:04 +0300

    Lesson6, task3 - implementation

--- src/main/java/lesson6/task3/Main.java
+++ src/main/java/lesson6/task3/Main.java
@@ -2,6 +2,11 @@ package lesson6.task3;
 
 import java.util.Arrays;
 
+/*
+Создайте несколько классов, один класс наследует другой и использует аннотацию Inherited.
+Помимо этого, попробуйте ещё в проекте использовать 5 различных аннотаций.
+*/
+
 public class Main {
 
     public static void main(String[] args) {
